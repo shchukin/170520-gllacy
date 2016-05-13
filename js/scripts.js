192317a852a36b8cc8a6b4d7c6f113dda8c55a4c
Anton Shchukin <a.a.shchukin@gmail.com>
2016-05-13 13:34:00 +0700

    Combine scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,100 @@
+/*
+ * Browser
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var $html = document.querySelector('html');
+
+  function getBrowser() {
+    if ( (navigator.userAgent.indexOf('WebKit')+1 ) && !(navigator.userAgent.indexOf('Edge')+1) ) {
+      $html.classList.add('webkit');
+    } else if ( navigator.userAgent.indexOf( 'Edge' ) + 1 ) {
+      $html.classList.add('edge');
+    } else if ( navigator.userAgent.indexOf( 'Firefox' ) + 1 ) {
+      $html.classList.add('firefox');
+    }
+  }
+
+  getBrowser();
+
+});
+
+
+/*
+ * Input
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var inputElements = document.querySelectorAll('.input__control');
+  [].forEach.call(inputElements, function (item) {
+    item.addEventListener('input', function () {
+      if ( this.value.length ) {
+        this.parentNode.classList.add('input_fill');
+      } else  {
+        this.parentNode.classList.remove('input_fill');
+      }
+    })
+  })
+});
+
+
+/*
+ * Feedback
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var popup;
+
+  var feedback = {};
+  var feedbackLocal = {};
+
+  popup = document.querySelector('.popup_feedback');
+
+  feedback.form = document.querySelector('#feedback');
+  feedback.name = feedback.form.querySelector('#feedback-name');
+  feedback.email = feedback.form.querySelector('#feedback-email');
+  feedback.message = feedback.form.querySelector('#feedback-message');
+
+  feedbackLocal.name = localStorage.getItem('feedback-name');
+  feedbackLocal.email = localStorage.getItem('feedback-email');
+
+
+  if ( feedbackLocal.name ) {
+    feedback.name.value = feedbackLocal.name;
+  }
+  if ( feedbackLocal.email ) {
+    feedback.email.value = feedbackLocal.email;
+  }
+
+  feedback.form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    /* look for empty field and inform user */
+    if( !feedback.name.value || !feedback.email.value || !feedback.message.value ) {
+      popup.classList.add('popup_error');
+      alert('Пожалуйста, заполните все поля!');
+      return;
+    }
+
+    localStorage.setItem('feedback-name', feedback.name.value);
+    localStorage.setItem('feedback-email', feedback.email.value);
+
+    alert('Спасибо! Ваше сообщение отправлено.');
+
+    feedback.form.submit();
+
+  });
+
+});
+
+
+/*
+ * Popup
+ */
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
   /* Page lock */
@@ -101,4 +198,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 });
+
+
+
 
